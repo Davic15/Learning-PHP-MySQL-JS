934872543dc97937bbe7ecda5f92430bc065fa9c
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2022-03-06 23:09:28 +0100

    Chapter 3: PHP examples

---
+++ chapter3/example3-12.php
@@ -0,0 +1,8 @@
+<?php
+    function longdate($timestamp) {
+        return date("l F jS Y", $timestamp);
+    }
+
+    echo longdate(time());
+    echo longdate(time() - 17 * 24 * 60 *60);
+?>
---
+++ chapter3/example3-18.php
@@ -0,0 +1,7 @@
+<?php
+    function test() {
+        static $int = 0;            //allow
+        static $int = 1 + 2;        //disallowed
+        static $int = sqrt(144);    //disallowed
+    }
+?>
