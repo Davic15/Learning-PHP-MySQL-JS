6fee1febb1f3fd62bb2a295d39cb9fe49b5a83e6
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2022-03-07 18:56:40 +0100

    Chapter 3: PHP examples

--- chapter3/example3-18.php
+++ chapter3/example3-18.php
@@ -2,6 +2,6 @@
     function test() {
         static $int = 0;            //allow
         static $int = 1 + 2;        //disallowed
-        static $int = sqrt(144);    //disallowed
+        //static $int = sqrt(144);    //disallowed
     }
 ?>
