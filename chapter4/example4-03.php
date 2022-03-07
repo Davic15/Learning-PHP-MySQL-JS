ec4481a1615d7b996fe217a401f39bfcb3197f64
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2022-03-07 19:30:51 +0100

    Chapter 4: PHP examples

---
+++ chapter4/example4-03.php
@@ -0,0 +1,10 @@
+<?php
+    $myname = "Brian";
+    $myage = 37;
+
+    echo "a: " . 73         .   "<br>";     //Numerica literal
+    echo "b: " . "Hello"    .   "<br>";     //String literal
+    echo "c: " . FALSE      .   "<br>";     //Constant literal
+    echo "d: " . $myname    .   "<br>";     //String variable
+    echo "e: " . $myage     .   "<br>";     //Numeric variable
+?>
