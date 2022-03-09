e0388bcd2a0ede7a7a61fd8cceba3b779ec0b2a5
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2022-03-09 13:08:11 +0100

    Chapter 5: PHP Functions and Objects

---
+++ chapter5/example5-01.php
@@ -0,0 +1,7 @@
+<?php
+    echo strrev("  .dlrow olleH");      //reversing a string
+    echo "<br>";
+    echo str_repeat("Hip ", 2);          //repeat string
+    echo "<br>";
+    echo strtoupper("hooray!");         //string to uppercase
+?>
