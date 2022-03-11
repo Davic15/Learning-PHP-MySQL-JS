d2b174e79e9aa6d28003987e602d939da4c7da8d
Franklin David Macias Avellan <franklindavid.maciasavellan@gmail.com>
2022-03-11 17:18:27 +0100

    Chapter 7: Practical PHP

---
+++ chapter7/example7-17.php
@@ -0,0 +1,10 @@
+<?php
+    $cmd = "dir";
+    exec(escapeshellcmd($cmd), $output, $status);
+    if($status) echo "Exec command failed";
+    else {
+        echo "<pre>";
+        foreach($output as $line) echo htmlspecialchars("$line\n");
+        echo "</pre>";
+    }
+?>
